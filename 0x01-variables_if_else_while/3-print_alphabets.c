7da580fedb6e9d2adc6275424239db10fab705bc
nati <nati@gmail.com>
2022-03-11 13:56:55 -0800

    Task 3_0

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main -Prints alphabets
+*
+* Return: Always(Success)
+*/
+
+int main(void)
+{
+char n;
+
+for (n = 'a'; n <= 'z'; n++)
+{
+	putchar(n);
+}
+
+for (n = 'A'; n <= 'Z'; n++)
+{
+	putchar(n);
+}
+
+	putchar('\n');
+	
+	return (0);
+}
