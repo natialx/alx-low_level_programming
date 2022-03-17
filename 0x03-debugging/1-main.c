dedf3c8652d98bfcf71c216a65c87f8b33fac225
nati <nati@gmail.com>
2022-03-17 12:17:54 -0700

    Task1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* main - comment out couse of infinite loop
+* Return: 0
+*/
+
+int main(void)
+{
+int i;
+
+printf("Infinite loop incoming :(\n");
+
+i = 0;
+
+/*
+*while (i < 10)
+*{
+*Infinite loop - no increment
+*putchar(i);
+}
+*/
+printf("Infinite loop avoided! \\o/\n");
+return (0);
+}
