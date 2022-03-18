a2219e121bb53eb7b901c43248c52c22f1e84d4b
nati <nati@gmail.com>
2022-03-18 12:41:10 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isdigit - print nums 0-9
+ * Return: Always 0
+ */
+
+void print_numbers(void)
+{
+int i;
+
+i=0;
+while (i < 10)
+{
+_putchar (i);
+i++;
+}
+
+_putchar ('\n');
+}
